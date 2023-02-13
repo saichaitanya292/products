15d79c7efbe88f829e639bdc87f3aac0932b79a2
saichaitanya292 <saichaitanya292@gmail.com>
2023-02-13 21:15:27 +0530

    shopc cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'products';
+  productList:any;
+  cartitems: Array<any> = [];
+  len:Number = 0;
+  constructor(private http: HttpClient) {
+    this.http.get('https://fakestoreapi.com/products').subscribe(data => this.productList = data);
+   
+  }
+
+  Addtocart(i:any)
+  {
+    console.log(i);
+    this.cartitems.push(i.title);
+    this.len = this.cartitems.length;
+    console.log(this.cartitems);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
